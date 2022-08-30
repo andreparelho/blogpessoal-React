5a3e1669e119c2ea0349ef0bebe966ff77365b19
Andre <parelhoneves.andre@gmail.com>
2022-08-30 19:02:15 -0300

    atualização do projeto react

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './components/static/footer/Footer';
 import Navbar from './components/static/navbar/Navbar';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <Home />
-    <Footer /> 
-    </>
+    <Router>
+      <Navbar />
+        <div style={{minHeight: '100vh'}} > 
+        <Routes>
+          <Route path= '/home' element={<Home />}  />
+          <Route path= '/' element={<Login />}  />
+          <Route path= '/login' element={<Login />}  />
+        </Routes>
+        </div>
+      <Footer />
+    </Router>
+    
   );
 }
 
