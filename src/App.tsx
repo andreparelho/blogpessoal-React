cf553f594abd0708f99e76992fc62f57bb737310
Andre <parelhoneves.andre@gmail.com>
2022-09-08 13:31:30 -0300

    atualização do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,58 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
-import Footer from './components/static/footer/Footer';
 import Navbar from './components/static/navbar/Navbar';
-import ListaTema from './components/temas/listatema/ListaTema';
+import Footer from './components/static/footer/Footer';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
+import './App.css';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 
 function App() {
   return (
     <Router>
       <Navbar />
-        <div style={{minHeight: '100vh'}} > 
+      <div style={{ minHeight: '100vh' }}>
         <Routes>
-          <Route path= '/home' element={<Home />}  />
-          <Route path= '/' element={<Login />}  />
-          <Route path= '/login' element={<Login />}  />
-          <Route path= '/cadastrousuario' element={<CadastroUsuario />}  />
-          <Route path= '/temas' element={<ListaTema />}  />
-          <Route path= '/postagens' element={<ListaPostagem />}  />
+
+          <Route path="/" element={<Login />} />
+
+
+
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/home" element={<Home />} />
+
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+
+          <Route path="/temas" element={<ListaTema />} />
+
+          <Route path="/postagens" element={<ListaPostagem />} />
+
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
+
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+          <Route path="/formularioTema" element={<CadastroTema />} />
+
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
+
+
         </Routes>
-        </div>
+      </div>
       <Footer />
+
     </Router>
-    
   );
 }
 
--- src/components/postagens/listapostagem/ListaPostagem.tsx
+++ src/components/postagens/listapostagem/ListaPostagem.tsx
@@ -1,50 +1,84 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Postagem from '../../../models/Postagem';
+import { busca } from '../../../services/Service'
 import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 import './ListaPostagem.css';
+import useLocalStorage from 'react-use-localstorage';
+import { useNavigate } from 'react-router-dom'
 
 function ListaPostagem() {
+  const [postagens, setPostagens] = useState<Postagem[]>([])
+  const [token, setToken] = useLocalStorage('token');
+  let history = useNavigate();
+
+  useEffect(() => {
+    if (token == "") {
+      alert("Você precisa estar logado")
+      history("/login")
+
+    }
+  }, [token])
+
+  async function getPost() {
+    await busca("/postagens", setPostagens, {
+      headers: {
+        'Authorization': token
+      }
+    })
+  }
+
+  useEffect(() => {
+
+    getPost()
+
+  }, [postagens.length])
 
   return (
     <>
-      <Box m={2} >
-        <Card variant="outlined">
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              Postagens
-            </Typography>
-            <Typography variant="h5" component="h2">
-              Título
-            </Typography>
-            <Typography variant="body2" component="p">
-              Texto da Postagem
-            </Typography>
-            <Typography variant="body2" component="p">
-              Tema
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Box display="flex" justifyContent="center" mb={1.5}>
+      {
+        postagens.map(postagens => (
+          <Box m={2} >
+            <Card variant="outlined">
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>
+                  Postagens
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {postagens.titulo}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {postagens.texto}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {postagens.tema?.descricao}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Box display="flex" justifyContent="center" mb={1.5}>
 
-              <Link to="" className="text-decorator-none" >
-                <Box mx={1}>
-                  <Button variant="contained" className="marginLeft" size='small' color="primary" >
-                    atualizar
-                  </Button>
+                  <Link to={`/formularioPostagem/${postagens.id}`} className="text-decorator-none" >
+                    <Box mx={1}>
+                      <Button variant="contained" className="marginLeft" size='small' color="primary" >
+                        atualizar
+                      </Button>
+                    </Box>
+                  </Link>
+                  <Link to={`/deletarPostagem/${postagens.id}`} className="text-decorator-none">
+                    <Box mx={1}>
+                      <Button variant="contained" size='small' color="secondary">
+                        deletar
+                      </Button>
+                    </Box>
+                  </Link>
                 </Box>
-              </Link>
-              <Link to="" className="text-decorator-none">
-                <Box mx={1}>
-                  <Button variant="contained" size='small' color="secondary">
-                    deletar
-                  </Button>
-                </Box>
-              </Link>
-            </Box>
-          </CardActions>
-        </Card>
-      </Box>
-    </>)
+              </CardActions>
+            </Card>
+          </Box>
+        ))
+      }
+    </>
+  )
 }
 
 export default ListaPostagem;
